fc1987356f7811453dc3f6229710abc6a70e41aa
Sasakid <24391001+Sasakid0123@users.noreply.github.com>
2021-11-04 22:59:26 +0900

    Add Control

--- node.js
+++ node.js
@@ -4,6 +4,48 @@ module.exports = function(RED) {
     const BeaconScanner = require('./scanner.js');
     const scanner = new BeaconScanner();
 
+    function BLEBeaconNodeControl(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        scanner.onadvertisement = (ad) => {
+	    if(ad !== null)
+	    {
+                node.send({
+                   payload: ad
+                });
+	    }
+        };
+
+        node.on('input', function(msg) {
+            if (msg.payload){
+                scanner.startScan().then(() => {
+                    node.status({
+                        fill: "green",
+                        shape: "dot",
+                        text: "Scanning Started"
+                    });
+                }).catch((error) => {
+                    node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: error
+                    });
+                });
+            } else {
+                scanner.stopScan();
+                done();
+            }
+        });
+
+        node.on('close', function(done) {
+            scanner.stopScan();
+            done();
+        });
+        
+    }
+    RED.nodes.registerType("BLE Beacon Scanner with Control",BLEBeaconNodeControl);
+
     function BLEBeaconNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
